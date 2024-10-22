0b376cf8cfe8249fe6a793f13b8b2c8423ee70e3
sunchuanhong <sunchuanhong@beisen.com>
2024-10-22 12:31:59 +0800

    feat:modify name

--- 水库项目出表/Form1.Designer.cs
+++ 水库项目出表/Form1.Designer.cs
@@ -446,7 +446,7 @@
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "水库项目出表";
+            this.Text = "建设用地报批勘测定界成果批量数据处理软件";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
